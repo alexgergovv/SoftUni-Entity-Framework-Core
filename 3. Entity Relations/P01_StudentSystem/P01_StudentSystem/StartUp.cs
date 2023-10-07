7e40586d003a5b7ce338abcb62b4f19531d6df16
Alex Gergov <alexgergovv@gmail.com>
2023-10-07 12:17:05 +0300

    Add files via upload

---
+++ 3. Entity Relations/P01_StudentSystem/P01_StudentSystem/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using P01_StudentSystem.Data;
+
+namespace P01_StudentSystem
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var context = new StudentSystemContext();
+        }
+    }
+}
