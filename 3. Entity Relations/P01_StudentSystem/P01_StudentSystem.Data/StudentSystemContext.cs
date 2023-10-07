8c9de1500c19f53c06d214d2319a8b2a2a7904de
Alex Gergov <alexgergovv@gmail.com>
2023-10-07 12:18:48 +0300

    Add files via upload

---
+++ 3. Entity Relations/P01_StudentSystem/P01_StudentSystem.Data/StudentSystemContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Common;
+using P01_StudentSystem.Data.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+
+        }
+
+        public StudentSystemContext(DbContextOptions options)
+        : base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Homework> Homeworks { get; set; }
+        public DbSet<StudentCourse> StudentsCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(DbConfig.ConnectionString);
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<StudentCourse>(entity =>
+            {
+                entity.HasKey(sc => new { sc.StudentId, sc.CourseId });
+
+                entity.HasOne(sc => sc.Student)
+               .WithMany(s => s.StudentsCourses)
+               .HasForeignKey(sc => sc.StudentId);
+
+                entity.HasOne(sc => sc.Course)
+                    .WithMany(c => c.StudentsCourses)
+                    .HasForeignKey(sc => sc.CourseId);
+            });
+        }
+    }
+}
