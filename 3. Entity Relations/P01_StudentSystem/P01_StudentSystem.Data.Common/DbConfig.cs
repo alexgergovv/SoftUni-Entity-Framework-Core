d528fd37289bee749af7b13e0ba42d620505df56
Alex Gergov <alexgergovv@gmail.com>
2023-10-07 12:19:50 +0300

    Add files via upload

---
+++ 3. Entity Relations/P01_StudentSystem/P01_StudentSystem.Data.Common/DbConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace P01_StudentSystem.Data.Common
+{
+    public class DbConfig
+    {
+        public const string ConnectionString = @"SERVER=.\SQLExpress;Database=MinionsDB;
+        Integrated Security=true;TrustServerCertificate=true;";
+    }
+}
