b2728fd557c2a202a572d9def3942bc4fc10add6
Alex Gergov <alexgergovv@gmail.com>
2023-12-03 22:46:21 +0200

    Add files via upload

---
+++ 7. Xml Processing/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Export;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            //Supplier
+            this.CreateMap<ImportSupplierDto, Supplier>();
+            this.CreateMap<Supplier, ExportSupplierDto>()
+                .ForMember(d => d.PartsCount, opt => opt.MapFrom(s => s.Parts.Count));
+
+            //Part
+            this.CreateMap<ImportPartDto, Part>()
+                .ForMember(d => d.SupplierId, opt => opt.MapFrom(s => s.SupplierId.Value));
+            this.CreateMap<Part, ExportPartDto>();
+
+            //Car
+            this.CreateMap<ImportCarDto, Car>()
+                .ForSourceMember(s => s.Parts, opt => opt.DoNotValidate());
+            this.CreateMap<Car, ExportCarsDto>();
+            this.CreateMap<Car, ExportBmwCarDto>();
+            this.CreateMap<Car, ExportCarWithPartDto>()
+                .ForMember(d => d.Parts, opt => opt.MapFrom(s => s.PartsCars.Select(p => p.Part).OrderByDescending(p => p.Price)));
+            this.CreateMap<Car, ExportCarForSaleDto>();
+
+            //Customer
+            this.CreateMap<ImportCustomerDto, Customer>()
+                .ForMember(d => d.BirthDate, opt => opt.MapFrom(s => DateTime.Parse(s.BirthDate)));
+            this.CreateMap<Customer, ExportCustomerDto>()
+                .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.BoughtCars, opt => opt.MapFrom(src => src.Sales.Count))
+                .ForMember(dest => dest.SpentMoney,
+                opt => opt.MapFrom(src => src.Sales.Sum(s => s.Car.PartsCars.Sum(pc => pc.Part.Price))));
+
+
+            //Sale
+            this.CreateMap<ImportSaleDto, Sale>();
+            CreateMap<Sale, ExportSalesDto>()
+               .ForMember(dest => dest.CustomerName, opt => opt.MapFrom(src => src.Customer.Name))
+               .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Car.PartsCars.Sum(pc => pc.Part.Price)))
+               .ForMember(dest => dest.PriceWithDiscount, opt => opt.MapFrom(src =>
+                   src.Car.PartsCars.Sum(pc => pc.Part.Price) - (src.Car.PartsCars.Sum(pc => pc.Part.Price) * src.Discount / 100)));
+        }
+    }
+}
---
+++ 7. Xml Processing/CarDealer/StartUp.cs
@@ -0,0 +1,253 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CarDealer.Data;
+using CarDealer.DTOs.Export;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using CarDealer.Utilities;
+using Castle.Core.Resource;
+using System.IO;
+using System.Linq.Expressions;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+            Console.WriteLine(GetTotalSalesByCustomer(context));
+        }
+        //Problem 09
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = CreateMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+            ImportSupplierDto[] supplierDtos = xmlHelper.Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+
+            ICollection<Supplier> validSuppliers = new HashSet<Supplier>();
+            foreach (ImportSupplierDto supplierDto in supplierDtos)
+            {
+                if (string.IsNullOrEmpty(supplierDto.Name))
+                {
+                    continue;
+                }
+
+                Supplier supplier = mapper.Map<Supplier>(supplierDto);
+                validSuppliers.Add(supplier);
+            }
+
+            context.Suppliers.AddRange(validSuppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {validSuppliers.Count}";
+        }
+        //Problem 10
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = CreateMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+            ImportPartDto[] partDtos = xmlHelper.Deserialize<ImportPartDto[]>(inputXml, "Parts");
+
+            ICollection<Part> validParts = new HashSet<Part>();
+            foreach (ImportPartDto partDto in partDtos)
+            {
+                if (!partDto.SupplierId.HasValue || !context.Suppliers.Any(s => s.Id == partDto.SupplierId))
+                {
+                    continue;
+                }
+
+                if (string.IsNullOrEmpty(partDto.Name)) 
+                { 
+                    continue; 
+                }
+
+                Part part = mapper.Map<Part>(partDto);
+                validParts.Add(part);
+            }
+
+            context.Parts.AddRange(validParts);
+            context.SaveChanges();
+
+            return $"Successfully imported {validParts.Count}";
+        }
+        //Problem 11
+        public static string ImportCars(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = CreateMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+            ImportCarDto[] carDtos = xmlHelper.Deserialize<ImportCarDto[]>(inputXml, "Cars");
+
+            ICollection<Car> validCars = new HashSet<Car>();
+            foreach (ImportCarDto carDto in carDtos)
+            {
+                if(string.IsNullOrEmpty(carDto.Model) || 
+                   string.IsNullOrEmpty(carDto.Make))
+                {
+                    continue;
+                }
+
+                Car car = mapper.Map<Car>(carDto);
+                foreach (var part in carDto.Parts.DistinctBy(p => p.PartId))
+                {
+                    if(!context.Parts.Any(p => p.Id == part.PartId))
+                    {
+                        continue;
+                    }
+
+                    PartCar carPart = new PartCar()
+                    {
+                        PartId = part.PartId
+                    };
+                    car.PartsCars.Add(carPart);
+                }
+                validCars.Add(car);
+            }
+
+            context.Cars.AddRange(validCars);
+            context.SaveChanges();
+
+            return $"Successfully imported {validCars.Count}";
+        }
+        //Problem 12
+        public static string ImportCustomers(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = CreateMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+            ImportCustomerDto[] customerDtos = xmlHelper.Deserialize<ImportCustomerDto[]>(inputXml, "Customers");
+
+            ICollection<Customer> validCustomers = new HashSet<Customer>();
+            foreach (ImportCustomerDto customerDto in customerDtos)
+            { 
+                if(string.IsNullOrEmpty(customerDto.Name)) 
+                { 
+                    continue; 
+                }
+
+                Customer customer = mapper.Map<Customer>(customerDto);
+                validCustomers.Add(customer);
+            }
+
+            context.Customers.AddRange(validCustomers);
+            context.SaveChanges();
+
+            return $"Successfully imported {validCustomers.Count}";
+        }
+        //Problem 13
+        public static string ImportSales(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = CreateMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+            ImportSaleDto[] saleDtos = xmlHelper.Deserialize<ImportSaleDto[]>(inputXml, "Sales");
+
+            ICollection<Sale> validSales = new HashSet<Sale>();
+            foreach (ImportSaleDto saleDto in saleDtos)
+            {
+                if(!context.Cars.Any(c => c.Id == saleDto.CarId))
+                {
+                    continue;
+                }
+
+                Sale sale = mapper.Map<Sale>(saleDto);
+                validSales.Add(sale);
+            }
+
+            context.Sales.AddRange(validSales);
+            context.SaveChanges();
+
+            return $"Successfully imported {validSales.Count}";
+        }
+        //Problem 14
+        public static string GetCarsWithDistance(CarDealerContext context)
+        {
+            IMapper mapper = CreateMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ExportCarsDto[] cars = context.Cars
+                .Where(c => c.TraveledDistance > 2000000)
+                .OrderBy(c => c.Make)
+                .ThenBy(c => c.Model)
+                .ProjectTo<ExportCarsDto>(mapper.ConfigurationProvider)
+                .Take(10)
+                .ToArray();
+
+            return xmlHelper.Serialize<ExportCarsDto[]>(cars, "cars");
+        }
+        //Problem 15
+        public static string GetCarsFromMakeBmw(CarDealerContext context)
+        {
+            IMapper mapper = CreateMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ExportBmwCarDto[] cars = context.Cars
+                .Where(c => c.Make == "BMW")
+                .OrderBy(c => c.Model)
+                .ThenByDescending(c => c.TraveledDistance)
+                .ProjectTo<ExportBmwCarDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            return xmlHelper.Serialize<ExportBmwCarDto[]>(cars, "cars");
+        }
+        //Problem 16
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            IMapper mapper = CreateMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ExportSupplierDto[] suppliers = context.Suppliers
+                .Where(s => s.IsImporter == false)
+                .ProjectTo<ExportSupplierDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            return xmlHelper.Serialize<ExportSupplierDto[]>(suppliers, "suppliers");
+        }
+        //Problem 17
+        public static string GetCarsWithTheirListOfParts(CarDealerContext context)
+        {
+            IMapper mapper = CreateMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ExportCarWithPartDto[] cars = context.Cars
+                .OrderByDescending(c => c.TraveledDistance)
+                .ThenBy(c => c.Model)
+                .Take(5)
+                .ProjectTo<ExportCarWithPartDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            return xmlHelper.Serialize<ExportCarWithPartDto[]>(cars, "cars");
+        }
+        //Problem 18
+        public static string GetTotalSalesByCustomer(CarDealerContext context)
+        {
+            IMapper mapper = CreateMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            var customers = context.Customers
+                .Where(c => c.Sales.Any())
+                .ProjectTo<ExportCustomerDto>(mapper.ConfigurationProvider)
+                .OrderByDescending(c => c.SpentMoney)
+                .ToArray();
+
+            return xmlHelper.Serialize(customers, "customers");
+        }
+        //Problem 19
+        public static string GetSalesWithAppliedDiscount(CarDealerContext context)
+        {
+            IMapper mapper = CreateMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            var sales = context.Sales
+                .ProjectTo<ExportSalesDto>(mapper.ConfigurationProvider)
+                .ToArray();
+
+            return xmlHelper.Serialize<ExportSalesDto[]>(sales, "sales");
+        }
+        public static IMapper CreateMapper()
+        {
+            return new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
+        }
+    }
+}
