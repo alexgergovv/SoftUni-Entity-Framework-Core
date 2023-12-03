eb7c3db4d04436b44386a040a29f47fe6698d369
Alex Gergov <alexgergovv@gmail.com>
2023-12-03 14:55:42 +0200

    Add files via upload

---
+++ 7. Xml Processing/ProductShop/DTOs/Export/ExportUserDto.cs
@@ -0,0 +1,25 @@
+﻿using ProductShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Export
+{
+    [XmlType("User")]
+    public class ExportUserDto
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; } = null!;
+
+        [XmlElement("lastName")]
+
+        public string LastName { get; set; } = null!;
+
+        [XmlArray("soldProducts")]
+
+        public ExportSoldProductsDto[] ProductsSold { get; set; } = null!;
+    }
+}
---
+++ 7. Xml Processing/ProductShop/DTOs/Export/ExportUsersCountDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Export
+{
+    public class ExportUsersCountDto
+    {
+        [XmlElement("count")]
+        public int Count { get; set; }
+        [XmlArray("users")]
+        public List<ExportUsersAndProductsDto> Users { get; set; }
+    }
+}
