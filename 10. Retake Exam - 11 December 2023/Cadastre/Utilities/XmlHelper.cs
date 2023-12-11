cd8016c5f0998f5daf338aeb515acf35bce9c0a7
Alex Gergov <alexgergovv@gmail.com>
2023-12-11 23:43:19 +0200

    Add files via upload

---
+++ 10. Retake Exam - 11 December 2023/Cadastre/Utilities/XmlHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Cadastre.Utilities
+{
+    public class XmlHelper
+    {
+        public T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute root = new XmlRootAttribute(rootName);
+            XmlSerializer serializer = new XmlSerializer(typeof(T), root);
+
+            using StringReader xmlReader = new StringReader(inputXml);
+            T deserializedDto = (T)serializer.Deserialize(xmlReader);
+
+            return deserializedDto;
+        }
+        public IEnumerable<T> DeserializeCollection<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute root = new XmlRootAttribute(rootName);
+            XmlSerializer serializer = new XmlSerializer(typeof(T[]), root);
+
+            using StringReader reader = new StringReader(inputXml);
+            T[] deserializedDtos = (T[])serializer.Deserialize(reader);
+
+            return deserializedDtos;
+        }
+        public string Serialize<T>(T obj, string rootName)
+        {
+            StringBuilder sb = new StringBuilder();
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T), xmlRoot);
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter writer = new StringWriter(sb);
+            xmlSerializer.Serialize(writer, obj, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
